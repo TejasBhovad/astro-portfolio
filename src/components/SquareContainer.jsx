9dc488ef7f878c74175dd548a0581dcce3e5d897
Tejas Bhovad <tejasbhovad@gmail.com>
2023-10-06 21:13:24 +0530

    🌐 Added Skills and Tech page

--- src/components/SquareContainer.jsx
+++ src/components/SquareContainer.jsx
@@ -1,11 +1,34 @@
-import React from "react";
 import Java from "../components/icons/Java.jsx";
+import ReactJS from "./icons/ReactJS.jsx";
+import Python from "../components/icons/Python.jsx";
+import Nextjs from "../components/icons/Nextjs.jsx";
+import Vscode from "../components/icons/Vscode.jsx";
+import Github from "../components/icons/Github.jsx";
+import Vercel from "../components/icons/Vercel.jsx";
 import "../styles/square.scss";
 
 const SquareContainer = ({ type }) => {
   switch (type) {
-    case "Java":
+    case "java":
       var logo = <Java />;
+      break;
+    case "react":
+      var logo = <ReactJS />;
+      break;
+    case "python":
+      var logo = <Python />;
+      break;
+    case "nextjs":
+      var logo = <Nextjs />;
+      break;
+    case "vscode":
+      var logo = <Vscode />;
+      break;
+    case "github":
+      var logo = <Github />;
+      break;
+    case "vercel":
+      var logo = <Vercel />;
       break;
     default:
       var logo = <Java />;
---
+++ src/components/icons/Vercel.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Vercel() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+          height="80"
+          className="other"
+      preserveAspectRatio="xMidYMid"
+      viewBox="0 -17 256 256"
+    >
+      <path d="M128 0L256 221.705007 0 221.705007z"></path>
+    </svg>
+  );
+}
+
+export default Vercel;
